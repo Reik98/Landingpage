9130c40f0beeb919f63e9a12a7117eef1941c685
Reik98 <reikba@yahoo.de>
2025-06-04 16:38:33 +0200

    Update Landingpage.py

--- Landingpage.py
+++ Landingpage.py
@@ -100,8 +100,8 @@ st.markdown("""
 <div class="hero">
     <h1>Verändern Sie Ihre Organisation mit Künstlicher Intelligenz</h1>
     <p>Kulturwandel beginnt dort, wo Technologie auf Haltung trifft.</p>
-    <ul>
-        <li>In vielen Organisationen basieren Kultur- und Change-Modelle auf klassischen Paradigmen: stabile Systeme, lineares Denken, Planbarkeit. Doch KI verändert die Spielregeln: Entscheidungen werden datenbasiert, Rollen verschwimmen, Führung wird adaptiv. Viele der bisher genutzten Modelle (z. B. Senge, Kotter) brauchen ein radikales Update.</li>
+    <ul>In vielen Organisationen basieren Kultur- und Change-Modelle auf klassischen Paradigmen: 
+    stabile Systeme, lineares Denken, Planbarkeit. Doch KI verändert die Spielregeln: Entscheidungen werden datenbasiert, Rollen verschwimmen, Führung wird adaptiv. Viele der bisher genutzten Modelle (z. B. Senge, Kotter) brauchen ein radikales Update.</ul>
     <a href="#form" class="cta-button">Kostenfreies Erstgespräch buchen</a>
 </div>
 """, unsafe_allow_html=True)
