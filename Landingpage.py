f5eb36c46c346ba71d0aa10dd3fb18aebe92a480
Reik98 <reikba@yahoo.de>
2025-06-04 17:16:27 +0200

    Update Landingpage.py

--- Landingpage.py
+++ Landingpage.py
@@ -103,11 +103,15 @@ st.markdown("""
         text-decoration: none;
     }
         .feature-box {
-            background-color: #ffffff;
+        background-color: #ffffff;
             padding: 1.5rem;
             border-radius: 12px;
             box-shadow: 0 2px 6px rgba(0,0,0,0.1);
-            margin-bottom: 1.5rem;
+            text-align: center; /* Text und Inhalte zentrieren */
+            display: flex;
+            flex-direction: column;
+            align-items: center; /* Inhalte horizontal zentrieren */
+            justify-content: center; /* Inhalte vertikal zentrieren, falls nötig */
         }
         .feature-box h4 {
             margin-top: 0;
