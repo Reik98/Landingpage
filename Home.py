a81903e637346fe53433f611a6da1ac356e56c3d
Reik98 <reikba@yahoo.de>
2025-06-04 20:04:57 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ from shared.header import show_header  # Gemeinsamer Header
 
 st.set_page_config(
     page_title="Kulturwandel durch KI – Ihre Organisationsberatung",
-    layout="wide"
+    layout="wide",
     initial_sidebar_state="collapsed"  # 👈 WICHTIG: Seitenleiste standardmäßig eingeklappt
 )
 
