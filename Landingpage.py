b7f9b20381bd5e4d15705bd6310831e7cc9c5cc5
Reik98 <reikba@yahoo.de>
2025-06-04 15:45:04 +0200

    Update Landingpage.py

--- Landingpage.py
+++ Landingpage.py
@@ -16,6 +16,8 @@ st.markdown("""
             position: fixed;
             top: 20px;
             left: 20px;
+            background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), 
+            url('https://raw.githubusercontent.com/Reik98/Landingpage/main/Logo.png');
             z-index: 1000;
         }
         .logo-container img {
