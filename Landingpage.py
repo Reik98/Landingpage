931d6671ce3710f1cdc630cf5f3b8caa95ec4546
Reik98 <reikba@yahoo.de>
2025-06-04 15:48:54 +0200

    Update Landingpage.py

--- Landingpage.py
+++ Landingpage.py
@@ -88,6 +88,13 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+# Logo oben links
+st.markdown("""
+<div class="logo-container">
+    <img src="https://raw.githubusercontent.com/Reik98/Landingpage/main/Logo.png" alt="Aicura Logo">
+</div>
+""", unsafe_allow_html=True)
+
 
 # Hero-Sektion mit Scroll-Link
 st.markdown("""
