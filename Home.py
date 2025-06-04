bf5865b039c4acf77ef09ce2ab949b0dd5f612fc
Reik98 <reikba@yahoo.de>
2025-06-04 20:17:13 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -33,7 +33,7 @@ st.markdown("""
         .catchfrase {
             background-color: #ffffff; width: 100%; height: 200px;
             margin-top: 50px; padding: 1.5rem;
-            text-align: center
+            text-align: center;
             border-radius: 12px; box-shadow: 0 2px 6px rgba(0,0,0,0.1);
             margin-bottom: 1.5rem;
         }
