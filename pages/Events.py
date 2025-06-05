228e70757729aa4859895fe9e990a97d3a910deb
Reik98 <reikba@yahoo.de>
2025-06-05 12:59:37 +0200

    Update Events.py

--- pages/Events.py
+++ pages/Events.py
@@ -19,7 +19,7 @@ st.markdown("""
     height: 500px;
     margin-top: 20px;
     background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),
-    url('https://raw.githubusercontent.com/Reik98/Landingpage/main/Events.pgn');
+    url('https://raw.githubusercontent.com/Reik98/Landingpage/main/Events.png');
     background-size: cover;
     background-position: center;
     display: flex;
