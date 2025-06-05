923b1ad61ce0e082ccc2cf9e328354fa232e6339
Reik98 <reikba@yahoo.de>
2025-06-05 14:15:54 +0200

    Update Über uns.py

--- pages/Über uns.py
+++ pages/Über uns.py
@@ -17,7 +17,7 @@ st.markdown("""
     width: 100%;
     height: 500px;
     background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),
-    url('https://raw.githubusercontent.com/Reik98/Landingpage/main/UeberUns_Hero.png');
+    url('https://raw.githubusercontent.com/Reik98/Landingpage/main/Über uns.jpg');
     background-size: cover;
     background-position: center;
     display: flex;
