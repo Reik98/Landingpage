2219beaf9076090f5fac7214e95789c013359523
Reik98 <reikba@yahoo.de>
2025-06-09 19:15:34 +0200

    Update Kulturdiagnostik_Integrationsstrategie.py

--- pages/Kulturdiagnostik_Integrationsstrategie.py
+++ pages/Kulturdiagnostik_Integrationsstrategie.py
@@ -32,11 +32,11 @@ st.markdown("""
 }
 .hero-about h1 {
     font-size: 3rem;
-    margin-top: 3.5rem;
+    margin-top: 2.5rem;
 }
 .hero-about p {
     font-size: 1.3rem;
-    margin-top: 20.5rem;
+    margin-top: 50.5rem;
 }
 </style>
 <div class="hero-about">
