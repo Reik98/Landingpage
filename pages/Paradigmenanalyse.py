e8ed1795ba57969eddc64506cc6115893e356f5d
Reik98 <reikba@yahoo.de>
2025-06-09 19:01:29 +0200

    Update Paradigmenanalyse.py

--- pages/Paradigmenanalyse.py
+++ pages/Paradigmenanalyse.py
@@ -18,7 +18,7 @@ st.markdown("""
     height: 700px;
     background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),
     url('https://raw.githubusercontent.com/Reik98/Landingpage/main/Paradigmawechsel.jpg');
-    background-size: contain;
+    background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
     display: flex;
