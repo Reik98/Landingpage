e041221c660bdb70b42b47e886d8a44c240f48eb
Reik98 <reikba@yahoo.de>
2025-06-09 19:28:26 +0200

    Update Change_Coaching_Impulsformate.py

--- pages/Change_Coaching_Impulsformate.py
+++ pages/Change_Coaching_Impulsformate.py
@@ -19,6 +19,8 @@ st.markdown("""
     height: 700px;
     background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),
     url('https://raw.githubusercontent.com/Reik98/Landingpage/main/Coaching.jpg');
+    margin-bottom: 2rem; /* 👈 Abstand zum nächsten Abschnitt */
+    margin-top: 1rem; /* 👈 Abstand zum nächsten Abschnitt */
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
